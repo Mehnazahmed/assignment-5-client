16d7f8d48419905f5ad5addd77ad558a2ec3c61a
mehnaz <mehnazchowdhury613@gmail.com>
2024-08-25 14:14:01 +0600

    first commit

---
+++ src/redux/features/facilitySlice.ts
@@ -0,0 +1,20 @@
+import { createSlice } from "@reduxjs/toolkit";
+import { TFacility } from "../../types";
+
+type TInitialState = {
+  facilities: TFacility[];
+};
+
+const initialState: TInitialState = {
+  facilities: [],
+};
+
+const facilitySlice = createSlice({
+  name: "Facilities",
+  initialState,
+  reducers: {},
+});
+
+export const {} = facilitySlice.actions;
+
+export default facilitySlice.reducer;
