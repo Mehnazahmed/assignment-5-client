ccccb3eb97e7643cfa154f0c84706012852ab84a
mehnaz <mehnazchowdhury613@gmail.com>
2024-09-22 19:11:37 +0600

    updated

--- src/pages/Shared/NotFound.tsx
+++ src/pages/Shared/NotFound.tsx
@@ -1,8 +1,12 @@
 import notFound from "@/assets/notFound.png";
+import { Link } from "react-router-dom";
 export default function NotFound() {
   return (
     <div className="mx-auto flex justify-center">
       <img src={notFound} alt=" not found page" />
+      <Link className="text-center" to="/">
+        Go to Home Page
+      </Link>
     </div>
   );
 }
